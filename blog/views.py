d6f9e943f02414a0bd4212b4205a00939dd30ca3
Rybasher <kirary99@gmail.com>
2020-04-04 05:30:50 +0300

    all changes

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -14,8 +16,33 @@ def courses_list(request):
 
 def courses_detail(request, slug):
     course = Course.objects.get(slug__iexact=slug)
+    # search_query = request.GET.get('search', '')
+    # if search_query:
+    #     posts = course.posts.filter(Q(title__contains=search_query) | Q(content__icontains=search_query))
+    #
+    # else:
+    #     posts = course.posts.all()
     posts = course.posts.all()
-    return render(request, 'blog/course_detail.html', {'course': course, 'posts': posts})
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        last_url = '?page={}'.format(paginator.page_range[-1])
+        next_url = '?page={}'.format(page.next_page_number())
+
+    else:
+        next_url = ''
+        last_url = ''
+    return render(request, 'blog/course_detail.html', {'course': course, 'posts': posts, 'is_paginated': is_paginated,
+                                                       'page_object': page, 'prev_url': prev_url, 'next_url': next_url,
+                                                       'last_url': last_url})
 
 
 def post_detail(request, slug):
